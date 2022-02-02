39830513a7aacf5ccc1eceba338da69d64100ae5
Mishanya_42 <mihakgma86@hotmail.com>
2022-02-02 16:09:30 +0700

    added new atribute in cliquefinder() class - show_cliques_on_graph ('no' - by default). if it == 'yes' so the method find_cliques_sorted() build an original graph plot with cliques marked in another color. some other fixes have been done also in this class...

--- CliqueFinderClass.py
+++ CliqueFinderClass.py
@@ -1,17 +1,30 @@
 from pandas import Series as pd_Series
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class CliqueFinder():
 
     """
     Ищет клики с заданным количеством вершин и
     возвращает в виде (метод ...)...
+    clique_vertex_num - число вершин в клике
+    vertex_num_equals:
+        'yes' - искать клики строго по определенному количеству вершин
+        'no' - искать клики по количеству вершин и более
+    show_cliques_on_graph - показать исходный граф с нанесенными на него кликами
     """
 
-    def __init__(self, graph, method: str='default', clique_vertex_num:int=2, vertex_num_equals: str='no'):
+    def __init__(self,
+                 graph,
+                 method: str='default',
+                 clique_vertex_num:int=2,
+                 vertex_num_equals:str='no',
+                 show_cliques_on_graph:str='no'):
         self.__graph = graph
         self.__method = method
         self.__clique_vertex_num = clique_vertex_num
         self.__vertex_num_equals = vertex_num_equals
+        self.__show_cliques_on_graph = show_cliques_on_graph
 
     def get_graph(self):
         return self.__graph
@@ -24,6 +37,9 @@ class CliqueFinder():
 
     def get_vertex_num_equals(self):
         return self.__vertex_num_equals
+
+    def get_show_cliques_on_graph(self):
+        return self.__show_cliques_on_graph
 
 
     def default_clique_algorithm(self, G):
@@ -78,6 +94,7 @@ class CliqueFinder():
         method = self.get_method()
         clique_vertex_num = self.get_clique_vertex_num()
         vertex_num_equals = self.get_vertex_num_equals()
+        show_cliques_on_graph = self.get_show_cliques_on_graph()
 
         # алгоритм
 
@@ -107,6 +124,10 @@ class CliqueFinder():
 
         self.show_cliques_info(cliques_not_intersected, orig_graph_vertexes_num)
 
+        if show_cliques_on_graph == 'yes':
+            graph_color_lst = self.get_nodes_color(list(graph.nodes()), list(vertex_set))
+            self.draw_graph_with_cliques(graph, graph_color_lst)
+
         return cliques_not_intersected
 
 
@@ -135,4 +156,28 @@ class CliqueFinder():
             print(f'Всего вершин в найденных кликах: <{len(found_vertexes_lst_extended)}> (шт.)')
             print(f'ИЗ НИХ Уникальных вершин: <{found_cliques_vertexes_unique}> (шт.)')
 
+
+    def draw_graph_with_cliques(self, G, nodes_color: list):
+        """
+        Данный метод рисует граф
+        :param G: граф, поданный на вход методу find_cliques_sorted
+        nodes_color: лист с расцветкой узлов исходного графа
+        """
+        plt.figure(figsize=(8, 8))
+        nx.draw(G, with_labels=True, font_size=10, node_color=nodes_color)
+
+
+    def get_nodes_color(self, graph_nodes, cliques_nodes):
+        """
+        Предназначен для цветовой разметки узлов исходного графа
+        :param graph_nodes: узлы исходного графа (лист)
+        :param cliques_nodes: узлы клик (лист)
+        :return: лист с разцветкой по узлам исходного графа
+        """
+        color_graph = 'yellow'
+        color_cliques = 'green'
+
+        nodes_color = [color_cliques if node in cliques_nodes else color_graph for node in graph_nodes]
+
+        return nodes_color
 
